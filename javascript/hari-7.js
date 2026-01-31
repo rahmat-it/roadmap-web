2e19afe5a217bf3f2bd98b90cb646e1f42ad51a6
Rahmat Haji Adnin <rahmathajiadnin426@gmail.com>
2026-01-31 12:02:33 +0700

    hari 7: function dengan return

---
+++ javascript/hari-7.js
@@ -0,0 +1,13 @@
+function tambah(a, b) {
+    return a + b;
+}
+
+let hasil = tambah(5, 7);
+console.log("Hasil penjumlahan:", hasil);
+
+function kali(a, b) {
+  return a * b;
+}
+
+hasil = kali(3, 4);
+console.log("Hasil perkalian", hasil);
